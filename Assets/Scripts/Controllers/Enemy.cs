f85f27dd69d5544773776f4720ca6b47054f06af
cherrycolaman <148461991+cherrycolaman@users.noreply.github.com>
2024-11-10 13:14:37 -0500

    Enemy periodically spawns missile

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -6,12 +6,19 @@ public class Enemy : MonoBehaviour
     public Transform playerTransform;
     public GameObject missilePrefab;
     public float speed;
+    private float timer;
     private void Update()
     {
+        timer += Time.deltaTime;
         transform.position += Vector3.left * speed * Time.deltaTime;
         if(transform.position.x > 14 || transform.position.x < -14)
         {
             speed = speed * -1;
+        }
+        if (timer > 3)
+        {
+            Instantiate(missilePrefab, transform.position, Quaternion.identity);
+            timer = 0;
         }
     }
 
