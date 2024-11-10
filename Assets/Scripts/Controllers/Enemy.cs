bf0deef98b12fb824b055ce6546c5def74521f32
cherrycolaman <148461991+cherrycolaman@users.noreply.github.com>
2024-11-09 19:24:36 -0500

    Enemy moves from left to right near top of screen

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform playerTransform;
+    public GameObject missilePrefab;
+    public float speed;
     private void Update()
     {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+        if(transform.position.x > 14 || transform.position.x < -14)
+        {
+            speed = speed * -1;
+        }
     }
 
 }
